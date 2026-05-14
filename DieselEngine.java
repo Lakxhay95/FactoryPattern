c53cf7446f5ffeb2b13a8bb6c6cb0671d5af9524
Lakxhay95 <glakshay687@gmail.com>
2026-05-14 10:07:02 +0530

    added print statement

--- DieselEngine.java
+++ DieselEngine.java
@@ -3,6 +3,7 @@ package com.genie.springdemo.loosecouple;
 public class DieselEngine implements IEnginer{
     @Override
     public int start() {
+        System.out.println("Petrol Engine Started....");
         return 1;
     }
 }
--- PetrolEngine.java
+++ PetrolEngine.java
@@ -4,6 +4,7 @@ public class PetrolEngine implements IEnginer{
 
     @Override
     public int start() {
+        System.out.println("Petrol Engine Started....");
         return 1;
     }
 }
